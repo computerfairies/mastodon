15ddebff3fdb0dccdfd3252b11d0c07af287f758
Erin Pinheiro <Lana-chan@users.noreply.github.com>
2018-10-03 11:01:13 -0300

    code for building spritesheets

--- emoji_tools/build_spritesheet.py
+++ emoji_tools/build_spritesheet.py
@@ -2,6 +2,7 @@
 import json
 import os
 import shutil
+from wand.image import Image
 
 with open('emoji_settings.json') as f:
   data = json.load(f)
@@ -14,11 +15,41 @@ with open(settings['sprite_input']) as f:
 with open(settings['copy_input']) as f:
   copy_map = json.load(f)
 
-def find_codepoint(codepoint):
+# EMOJI FILES STUFF
+def do_files():
+  count_found = 0
+  count_missing = 0
+  missing = []
+
+  # iterate over every emoji in map file
+  for emoji in copy_map.items():
+    #print(emoji[1])
+    codepoint = emoji[1]
+
+    if codepoint == None:
+      continue
+
+    filename = find_source_svg(codepoint)
+    
+    if filename != None:
+      count_found = count_found + 1
+      shutil.copy2(filename, os.path.join(settings['destination'], codepoint + '.svg'))
+    else:
+      count_missing += 1
+      missing += [codepoint]
+      #print("!! NOT FOUND! !!")
+  
+  print("found: {:d}".format(count_found))
+  print("missing: {:d}".format(count_missing))
+  if count_missing:
+    print(missing)
+
+def find_source_svg(codepoint):
+  if codepoint == None:
+    return None
+
   # check each source in order for emoji
   for source in sources:
-    codepoint_original = codepoint
-
     # replace to match source filename
     try:
       for key in source['replace']:
@@ -43,52 +74,66 @@ def find_codepoint(codepoint):
   
   return None
 
-# EMOJI FILES STUFF
-def do_files():
+# SPRITESHEET STUFF
+def do_spritesheet():
   count_found = 0
   count_missing = 0
   missing = []
 
+  # init spritesheet
+  # 1768 = 52x52 32px emojis with 1px padding on all sides each
+  spritesheet = Image(width=1768, height=1768)
+
   # iterate over every emoji in map file
-  for emoji in copy_map.items():
-    #print(emoji[1])
-    codepoint = emoji[1]
+  for emoji in emoji_map:
+    print(emoji['name'])
 
-    if codepoint == None:
-      continue
-
-    filename = find_codepoint(codepoint)
+    # find in svgs in public folder
+    filename = find_sprite_svg(emoji['unified'])
+    if filename == None:
+      filename = find_sprite_svg(emoji['non_qualified'])
     
     if filename != None:
       count_found = count_found + 1
-      shutil.copy2(filename, os.path.join(settings['destination'], codepoint + '.svg'))
+      add_to_spritesheet(filename, spritesheet, emoji['sheet_x'], emoji['sheet_y'])
     else:
       count_missing += 1
-      missing += [codepoint]
+      missing += [emoji['unified']]
       #print("!! NOT FOUND! !!")
-  
+
   print("found: {:d}".format(count_found))
   print("missing: {:d}".format(count_missing))
   if count_missing:
     print(missing)
 
-# SPRITESHEET STUFF
-def do_spritesheet():
-  # iterate over every emoji in map file
-  for emoji in emoji_map:
-    print(emoji['name'])
+  sprite_filename = os.path.join(settings['destination'], settings['sheet'])
+  png_image = spritesheet.make_blob("png32")
+  with open(sprite_filename, "wb") as out:
+    out.write(png_image)
 
-    # try unified then non qualified
-    filename = find_codepoint('unified')
-    if filename == None: find_codepoint('non_qualified')
-    
-    if filename != None:
-      count_found = count_found + 1
-    else:
-      count_missing += 1
-      missing += [emoji['unified']]
-      print("!! NOT FOUND! !!")
+def find_sprite_svg(codepoint):
+  if codepoint == None:
+    return None
+
+  ncodepoint = codepoint.lower().lstrip("0")
+
+  # return filename if emoji exists in public
+  filename = os.path.join(settings['destination'], ncodepoint + ".svg")
+
+  if os.path.exists(filename):
+    return filename
+  
+  return None
+
+def add_to_spritesheet(filename, spreadsheet, x, y):
+  im = Image(filename=filename)
+  im.transform(resize="32x32")
+  spreadsheet.composite(im, x*34+1, y*34+1)
+  pass
 
 if __name__ == "__main__":
   if settings['copy_sources']:
+    print("Finding source SVGs to copy to {:s}".format(settings['destination']))
     do_files()
+  print("Rebuilding spritesheet...")
+  do_spritesheet()
